c7798fdf2aa28b3144260e84c1eab3b120ffd5aa
jervin <jeervin@gmail.com>
2020-06-14 13:59:08 -0500

    #2: Adding the command line function

--- src/main/java/org/jervin/FoodTruckFinder/FoodTruckFinderApplication.java
+++ src/main/java/org/jervin/FoodTruckFinder/FoodTruckFinderApplication.java
@@ -1,12 +1,44 @@
 package org.jervin.FoodTruckFinder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.jervin.FoodTruckFinder.model.FoodTruck;
+import org.jervin.FoodTruckFinder.model.FoodTruckFinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FoodTruckFinderApplication {
 
-	public static void main(String[] args) {
+	public static Options commandLineOptions() {
+		Options options = new Options();
+		options.addOption("lat", true,"Latitude" );
+		options.addOption("lon", true,"Longitude" );
+		options.addOption("foodType", true, "Food type");
+		options.addOption("mi", true, "Radius of search in miles");
+		return options;
+	}
+
+	public static void main(String[] args) throws Exception {
+		CommandLineParser parser = new DefaultParser();
+		CommandLine commandLine = parser.parse(commandLineOptions(), args);
+
+		if (commandLine.hasOption("lat") && commandLine.hasOption("lon")) {
+			double latitude = Double.parseDouble(commandLine.getOptionValue("lat"));
+			double longitude = Double.parseDouble(commandLine.getOptionValue("lon"));
+			String foodItems = commandLine.hasOption("foodType") ? commandLine.getOptionValue("foodType") : "";
+			double radius = commandLine.hasOption("mi") ? Double.parseDouble(commandLine.getOptionValue("mi")) : 1.0d;
+			List<FoodTruck> trucks = FoodTruckFinder.findTrucks(latitude, longitude, foodItems, radius);
+			final ObjectMapper mapper = new ObjectMapper();
+			System.out.println(mapper.writeValueAsString(trucks));
+			System.exit(0);
+		}
+
 		SpringApplication.run(FoodTruckFinderApplication.class, args);
 	}
 
--- src/main/java/org/jervin/FoodTruckFinder/controller/FoodTruckFinderController.java
+++ src/main/java/org/jervin/FoodTruckFinder/controller/FoodTruckFinderController.java
@@ -2,6 +2,7 @@ package org.jervin.FoodTruckFinder.controller;
 
 import org.jervin.FoodTruckFinder.model.CSVReader;
 import org.jervin.FoodTruckFinder.model.FoodTruck;
+import org.jervin.FoodTruckFinder.model.FoodTruckFinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,6 @@ public class FoodTruckFinderController {
                                       @RequestParam(name="long", required=true) double longitude,
                                       @RequestParam(name="foodType", defaultValue = "") String foodItems,
                                       @RequestParam(name="mi", defaultValue = "1.0") double distance) throws IOException {
-        List<FoodTruck> trucks = new CSVReader().getTrucks();
-        return trucks.stream()
-                .filter(truck -> DistanceCalculator.distance(latitude, longitude, truck.getLatitude(), truck.getLongitude()) <= distance )
-                .filter(truck -> truck.getFoodItems().toLowerCase().contains(foodItems.toLowerCase()))
-                .collect(Collectors.toList());
+        return FoodTruckFinder.findTrucks(latitude, longitude, foodItems, distance);
     }
 }
