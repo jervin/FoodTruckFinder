fe52b68d56d2d76e8722cef06a53df571fd8c5cb
jervin <jeervin@gmail.com>
2020-06-21 17:04:40 -0500

    Added a smoke test.

--- src/test/java/org/jervin/FoodTruckFinder/FoodTruckFinderApplicationTests.java
+++ src/test/java/org/jervin/FoodTruckFinder/FoodTruckFinderApplicationTests.java
@@ -1,13 +1,29 @@
 package org.jervin.FoodTruckFinder;
 
+import org.jervin.FoodTruckFinder.controller.FoodTruckFinderController;
+import org.jervin.FoodTruckFinder.model.FoodTruck;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootTest
 class FoodTruckFinderApplicationTests {
+	@Autowired
+	FoodTruckFinderController controller;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(controller);
 	}
 
+	@Test
+	void smokeTest() throws IOException {
+		List<FoodTruck> trucks = controller.findTrucks(37.78, -122.43, "", 1.0d);
+		Assertions.assertNotNull(trucks);
+		Assertions.assertFalse(trucks.isEmpty());
+	}
 }
